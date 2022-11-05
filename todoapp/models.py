dc26757c571a7e4c50572ca40adaeb7431ef9c54
ZARIFMAC <uzbzarif@gmail.com>
2022-11-05 17:02:04 +0500

    todoitem model

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class TodoItem(models.Model):
+    """
+    A todo item
+    """
+    title = models.CharField(max_length=255)
+    task = models.TextField()
+    status = models.BooleanField(default=False)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)    
+
+    def __str__(self):
+        return self.title
