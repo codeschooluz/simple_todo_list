e01a3442869786a6388d281e79ef56e0414965c9
ZARIFMAC <uzbzarif@gmail.com>
2022-11-05 17:22:00 +0500

    get url

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from todoapp.views import AddTodoItem
+from todoapp.views import AddTodoItem, GetAllTodoItems
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('add/', AddTodoItem.as_view())
+    path('add/', AddTodoItem.as_view()),
+    path('get/', GetAllTodoItems.as_view()),
+
 ]
