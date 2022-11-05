46aeb6b13379642fe600cc8dcedf0646df54bc17
ZARIFMAC <uzbzarif@gmail.com>
2022-11-05 17:25:30 +0500

    remove;

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
-from todoapp.views import AddTodoItem, GetAllTodoItems
+from todoapp.views import AddTodoItem, GetAllTodoItems,RemoveTodoItem
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('add/', AddTodoItem.as_view()),
     path('get/', GetAllTodoItems.as_view()),
+    path('remove/<int:id>/', RemoveTodoItem.as_view()),
 
 ]
